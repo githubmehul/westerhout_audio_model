2f8d414f9e3f8cce02ce6eb672cafed90b02c2eb
JS <52767191+JS29G@users.noreply.github.com>
2021-11-29 14:05:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ new Vue({
             tracks: [
                 {
                     //temp holder
-                    name: "Mekanın Sahibi",
-                    artist: "Norm Ender",
+                    name: "The Westerhout Family,
+                    artist: "Westerhout",
                     cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
                     source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
                     url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
